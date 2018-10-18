7dc9e85e6074908fbe745c47844dd2014fc61ce1
ringo199 <notbadapple@gmail.com>
2018-10-18 18:27:10 +0800

    测试

--- src/routes/videopage.js
+++ src/routes/videopage.js
@@ -1,34 +1,26 @@
-import React from 'react';
-// import { connect } from 'dva';
-// import { Button, Select } from 'antd';
-// import PropTypes from 'prop-types';
-import moment from 'moment';
-import styles from './videobody.less';
-import Xgplayer from 'xgplayer-react';
+import React, { Component } from 'react';
+import VideoPlay from './videoplay';
 
-const config = {
-    id: 'pili',
-    url: '/video/1.mp4',
-    poster: '/img/1.jpg',
-    pip: true,
-    bullet: {
-        switch: 'on', //开启'on',关闭'off'
-        url: './danmu.json',
-        method: 'get'
-    },
-};
-const VideoPage = () => (
-    <div className={styles.video_body}>
-        <Xgplayer config={config} format={'mp4'} playerInit={Player => Player} />
-        <div className={styles.video_body_danmulist}>
-            弹幕列表<br/>
-            <div>时间&emsp;弹幕内容&emsp;发送时间</div>
-            {require('../../danmu.json').data.map(
-                item =>
-                <div>{moment(item.offset_time).format('mm:ss')}&emsp;{item.text}&emsp;{moment(item.create_time).format('MM-DD hh:mm')}</div>
-            )}
-        </div>
-    </div>
-    )
+class Videopage extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        width: '960px',
+        height: '360px',
+      };
+    }
+    render() {
+        const { width, height } = this.state;
+        return (
+            <VideoPlay
+                width={width}
+                height={height}
+                onChangeWidth={e => this.setState({ width: e })}
+                onChangeHeight={e => this.setState({ height: e })}
+                {...this.props}
+            />
+        )
+    }
+}
 
-export default VideoPage;
+export default Videopage;
